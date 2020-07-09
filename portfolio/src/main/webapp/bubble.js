4720633532696bf42700387debb074eb40f93872
Murphy-OrangeMud <chengzhiyi_2@163.com>
2020-07-09 11:53:41 +0800

    modify bubble click animation

--- portfolio/src/main/webapp/bubble.js
+++ portfolio/src/main/webapp/bubble.js
@@ -1,17 +1,19 @@
 var id = ["cycling and traveling", "books/movies/music", "programming newbie"];
-//var xPos = [100,200,300];
-//var yPos = [300,400,500];
-var xPos = [50, document.body.clientWidth - 20, document.body.clientWidth / 2];
-var yPos = [document.body.clientHeight - 20, 50, document.body.clientHeight - 20];
+var xPos = [50, document.body.clientWidth - 20, document.body.clientWidth - 20];
+var yPos = [document.body.clientHeight - 20, 50, document.body.clientHeight / 2];
 var step = 1;
 var delay = [8, 10, 12];
-var height = [document.body.clientHeight, document.body.clientHeight, document.body.clientHeight];
-var width = [document.body.clientWidth, document.body.clientWidth, document.body.clientWidth];
+var height_min = [0,0,0];
+var width_min = [0,0,0];
+//var height_min = [document.body.clientHeight - 200, 0, document.body.clientHeight / 2 - 100];
+//var width_min = [0, document.body.clientWidth - 200, document.body.clientWidth - 200];
+var height = [document.body.clientHeight, 200, document.body.clientHeight / 2 + 100];
+var width = [200, document.body.clientWidth, document.body.clientWidth];
 var Hoffset = [0, 0, 0];
 var Woffset = [0, 0, 0];
 var yon = [0, 0, 0];
 var xon = [0, 0, 0];
-var pause = [true, true, true];
+var pause = [false, false, false];
 var interval = [];
 for (var i = 0; i < 3; i++) {
     document.getElementById(id[i]).style.left = xPos[i] + "px";
@@ -31,16 +33,16 @@ function changePos(button_id) {
     else xPos[button_id] -= step;
     //yon[button_id] = Math.random() % 10 >= 2? 1: 0;
     //xon[button_id] = Math.random() % 10 >= 2? 1: 0;
-    if (yPos[button_id] < 0) {
+    if (yPos[button_id] < height_min[button_id]) {
         yon[button_id] = 1;
-        yPos[button_id] = 0;
+        yPos[button_id] = height_min[button_id];
     }
     if (yPos[button_id] >= (height[button_id] - Hoffset[button_id])) {
         yon[button_id] = 0;
         yPos[button_id] = (height[button_id] - Hoffset[button_id]);
     }
-    if (xPos[button_id] < 0) {
-        xPos[button_id] = 0;
+    if (xPos[button_id] < width_min[button_id]) {
+        xPos[button_id] = width_min[button_id];
         xon[button_id] = 1;
     }
     if (xPos[button_id] >= (width[button_id] - Woffset[button_id])) {
--- portfolio/src/main/webapp/setframe.js
+++ portfolio/src/main/webapp/setframe.js
@@ -1,6 +1,8 @@
 function setframe() {
     document.body.clientHeight = screen.availHeight;
     document.body.clientWidth = screen.availWidth;
+    document.getElementByClassName("bg").height = screen.availHeight;
+    document.getElementByClassName("bg").width = screen.availWidth;
     document.getElementsByClassName("main").height = screen.availHeight;
     document.getElementsByClassName("main").width = screen.availWidth;
 }
