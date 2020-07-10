7ef7358eb7af84bf6ab70fcdfd4701dd633e31a9
Murphy-OrangeMud <chengzhiyi_2@163.com>
2020-07-10 21:08:13 +0800

    fixed the issues

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -47,9 +47,12 @@ import com.google.gson.Gson;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+
+    private boolean commented = false;
+
     public class Comment {
         public String timestamp;
-        public String email; // to be revised
+        public String email;
         public String content;
         
         public Comment() {}
@@ -66,6 +69,12 @@ public class DataServlet extends HttpServlet {
     PreparedQuery results = datastore.prepare(query);
 
     response.setContentType("text/html; charset=UTF-8");
+
+    if (commented) {
+        response.getWriter().println("<script>alert(\"感谢你的支持！\");</script>");
+        commented = false;
+    }
+
     for (Entity comments: results.asIterable()) {
         String timestamp = (String) comments.getProperty("timestamp");
         int year = Integer.parseInt(timestamp.substring(0,4));
@@ -73,7 +82,6 @@ public class DataServlet extends HttpServlet {
         int day = Integer.parseInt(timestamp.substring(8,10));
         if (year <= 2020 && month <= 7 && day <= 10) continue;
         response.getWriter().println(timestamp + "<br></br>");
-        // if (timestamp)
         String tmp_email = (String) comments.getProperty("email");
         int nickname = tmp_email.indexOf('@');
         if (nickname != -1) { 
@@ -90,7 +98,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      String comment = getParameter(request, "text-input", "");
+      String comment = JSoup.clean(request.getParameter("text-input"), Whitelist.none());
       String email;
       boolean Friend = Boolean.parseBoolean(getParameter(request, "friend", "false"));
 
@@ -114,6 +122,8 @@ public class DataServlet extends HttpServlet {
 
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(commentEntity);
+
+      commented = true;
       response.sendRedirect("/index.html");
   }
 
